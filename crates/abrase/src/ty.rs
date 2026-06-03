d95fdc03702781c8a0d926a4bd8520f6b86441c7
Lingxiao <knh190@hotmail.com>
2026-06-03 11:51:31 +0800

    Move tests out of src/

--- crates/abrase/src/ty.rs
+++ crates/abrase/src/ty.rs
@@ -74,26 +74,3 @@ impl Type {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn verify_ownership_derivation() {
-        assert_eq!(Type::Int.ownership(), Ownership::Copy);
-        assert_eq!(Type::String.ownership(), Ownership::Move);
-        assert_eq!(
-            Type::Tuple(vec![Type::Int, Type::Bool]).ownership(),
-            Ownership::Copy
-        );
-        assert_eq!(
-            Type::Tuple(vec![Type::Int, Type::String]).ownership(),
-            Ownership::Move
-        );
-        assert_eq!(
-            Type::Reference { is_mut: false, inner: Box::new(Type::String) }.ownership(),
-            Ownership::Copy
-        );
-    }
-}
